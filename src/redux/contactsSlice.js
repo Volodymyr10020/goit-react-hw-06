e439fb6017d5b1fbd2f6a06cc5055be6db3474a2
Vova <volodimir.komarniczky@gmail.com>
2025-01-31 20:38:33 +0000

    react hw6

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,21 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+  name: "",
+};
+
 const contactsSlice = createSlice({
   name: "contacts",
-  initialState: {
-    items: [],
-  },
+  initialState,
   reducers: {
-    addContact: (state, action) => {
-      state.items.push(action.payload);
+    addContact(state, action) {
+      const { id, name, number } = action.payload;
+      state.items.push({ id, name, number });
     },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
-      );
+    deleteContact(state, action) {
+      const contactId = action.payload;
+      state.items = state.items.filter((contact) => contact.id !== contactId);
+    },
+    setFilter(state, action) {
+      state.name = action.payload;
     },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
+
+export const selectContacts = (state) => state.contacts.items;
+export const selectFilter = (state) => state.contacts.name;
+
 export default contactsSlice.reducer;
